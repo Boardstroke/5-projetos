e752e4ebbaa9b4fd135f5f1633caf6c48acaf6fe
Boardstroke <matheus.fachinis@gmail.com>
2019-05-05 17:49:29 -0300

    Execução infinita e melhora no visual

--- game-of-life/app.py
+++ game-of-life/app.py
@@ -8,7 +8,8 @@
 
 import numpy as np 
 from copy import deepcopy
-
+import os as os 
+import time as time
 #Grid quadrada com 50 pontos
 N = 10
 #Célula viva == 1
@@ -31,7 +32,7 @@ class Jogo():
                 if(self.grid[i][j] == 1):
                     print("▄", end =" "),
                 elif(self.grid[i][j] == 0):
-                    print(" ", end=" ")
+                    print("•", end=" ")
             
             print(" ")
 
@@ -88,21 +89,38 @@ class Jogo():
 
         return t
 
+
+    def jogar(self):
+        
+        while(True):
+            os.system("clear")
+            print("!----------------------------------------!")
+            print("!-------------Jogo da vida---------------!")
+            print("!----------------------------------------!")
+            print("\n")
+           
+            self.mostrar()
+            self.verificar()
+            time.sleep(2)
+            
+            
+
     
 if __name__ == "__main__":
     interface = Jogo(N)
     inicial = [[4,5],[5,6],[6,4],[6,5],[6,6]]
-    
+    # inicial = [[9,7],[9,8],[9,9]]
+    # inicial = [[4,2],[5,3],[6,1],[6,2],[6,3]]            
     interface.estado_inicial(inicial)
-
-    interface.mostrar()
-    interface.verificar()
+    interface.jogar()
+    
+    
     
 
     #TODO:
         #Implementar mundo infinito
-            #A artimetica modular pode não estar sendo usada corretamente
-        #Implentar loop principal
+            # A artimetica modular pode não estar sendo usada corretamente
+            # Existe um caso especifico [[4,5],[5,6],[6,4],[6,5],[6,6]] em que quando chega na ponta as celulas vivas se separam uma em cada diagonal
         #Tentar receber o estado inicial de uma maneira mais simples
         #Tentar escrever alguma unidades de teste 
     
